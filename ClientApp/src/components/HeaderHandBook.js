ea75e766351d467b058443d0d097e4108b5c8825
Vladislav Chursin <Vladislav_Chursin@epam.com>
2020-07-17 13:21:39 +0400

    add Material-UI Table

---
+++ ClientApp/src/components/HeaderHandBook.js
@@ -0,0 +1,21 @@
+﻿import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+
+export class HeaderHandBook extends Component {
+    static displayName = HeaderHandBook.name;
+
+    render() {
+        return (
+            <div>
+                <TextField id="filled-basic" label="Фильтр" variant="filled" className="field-wrap" />
+                <Button variant="contained" color="primary" className="field-wrap">
+                    Добавить
+                </Button>
+                <Button variant="contained" color="secondary" className="field-wrap">
+                    Удалить
+                </Button>
+            </div>
+        );
+    }
+}
---
+++ ClientApp/src/components/Home.js
@@ -0,0 +1,16 @@
+﻿import React, { Component } from 'react';
+import { HeaderHandBook } from './HeaderHandBook';
+import { EnhancedTable } from './MainTable';
+
+export class Home extends Component {
+  static displayName = Home.name;
+
+  render () {
+      return (
+          <div>
+              <HeaderHandBook />
+              <EnhancedTable />
+          </div>
+    );
+  }
+}
